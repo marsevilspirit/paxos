4a1d76689f3fc0dc03b4d2328b3580d177053c75
marsevilspirit <marsevilspirit@gmail.com>
2025-08-12 06:28:30 +0800

    feat(paxos): acceptor implement

--- acceptor.go
+++ acceptor.go
@@ -1,6 +1,9 @@
 package paxos
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
 
 type Acceptor struct {
 	lis net.Listener
@@ -15,4 +18,41 @@ type Acceptor struct {
 	acceptedValue any
 
 	learners []int
+}
+
+func (a *Acceptor) Perpare(args *MsgArgs, reply *MsgReply) error {
+	if args.Number > a.minProposal {
+		a.minProposal = args.Number
+		reply.Number = a.acceptedNumber
+		reply.Value = a.acceptedValue
+		reply.Ok = true
+	} else {
+		reply.Ok = false
+	}
+	return nil
+}
+
+func (a *Acceptor) Accept(args *MsgArgs, reply *MsgReply) error {
+	if args.Number >= a.minProposal {
+		a.minProposal = args.Number
+		reply.Number = a.acceptedNumber
+		reply.Value = a.acceptedValue
+		reply.Ok = true
+
+		for _, lid := range a.learners {
+			go func(learner int) {
+				addr := fmt.Sprintf("127.0.0.1:%d", learner)
+				args.From = a.id
+				args.To = learner
+				resp := new(MsgReply)
+				ok := call(addr, "Learner.Learn", args, resp)
+				if !ok {
+					return
+				}
+			}(lid)
+		}
+	} else {
+		reply.Ok = false
+	}
+	return nil
 }
